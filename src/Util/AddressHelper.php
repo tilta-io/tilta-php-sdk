148eb6b6aed8d5102972da8a804548b782f2f993
Frederik Rommel <frederik.rommel@webidea24.de>
2023-07-20 10:30:58 +0200

    add address-helper to split street and housenumber

---
+++ src/Util/AddressHelper.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * Copyright (c) Tilta Fintech GmbH
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Tilta\Sdk\Util;
+
+class AddressHelper
+{
+    /**
+     * @var string
+     */
+    public const STREETNUMBER_REGEX = '/^([a-zäöüß\s\d.,-]+?)\s*([\d]+(?:\s?[a-z])?(?:\s?[-|+\/]{1}\s?\d*)?\s*[a-z]?)$/iu';
+
+    public static function getStreetName(string $addressWithNumber): ?string
+    {
+        $matches = self::regexMatchAddress($addressWithNumber);
+
+        return $matches[1] ?? null;
+    }
+
+    public static function getHouseNumber(string $addressWithNumber): ?string
+    {
+        $matches = self::regexMatchAddress($addressWithNumber);
+
+        return $matches[2] ?? null;
+    }
+
+    private static function regexMatchAddress(string $addressWithNumber): array
+    {
+        preg_match(self::STREETNUMBER_REGEX, $addressWithNumber, $matches);
+
+        return $matches;
+    }
+}
---
+++ tests/Functional/Util/AddressHelperTest.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * Copyright (c) Tilta Fintech GmbH
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Tilta\Sdk\Tests\Functional\Util;
+
+use PHPUnit\Framework\TestCase;
+use Tilta\Sdk\Util\AddressHelper;
+
+class AddressHelperTest extends TestCase
+{
+    /**
+     * @dataProvider getSimpleAddressesToTest
+     */
+    public function testGetStreetName(string $input, string $expectedStreet, string $expectedHouseNumber): void
+    {
+        static::assertEquals($expectedStreet, AddressHelper::getStreetName($input));
+        static::assertEquals($expectedHouseNumber, AddressHelper::getHouseNumber($input));
+    }
+
+    public function getSimpleAddressesToTest(): array
+    {
+        return [
+            ['This is the street 123', 'This is the street', '123'],
+            ['This is the street123', 'This is the street', '123'],
+
+            ['This is the street 123a', 'This is the street', '123a'],
+            ['This is the street123a', 'This is the street', '123a'],
+
+            ['This is the street 123 a', 'This is the street', '123 a'],
+            ['This is the street123 a', 'This is the street', '123 a'],
+
+            ['This is the street 123a-g', 'This is the street', '123a-g'],
+            ['This is the street123a-g', 'This is the street', '123a-g'],
+
+            ['This is the street 123 a-g', 'This is the street', '123 a-g'],
+            ['This is the street123 a-g', 'This is the street', '123 a-g'],
+
+            ['This is the street 1a-g', 'This is the street', '1a-g'],
+            ['This is the street1a-g', 'This is the street', '1a-g'],
+
+            ['This is the street 1 a-g', 'This is the street', '1 a-g'],
+            ['This is the street1 a-g', 'This is the street', '1 a-g'],
+
+            ['This is the street 123', 'This is the street', '123'],
+            ['This-is the-street 123', 'This-is the-street', '123'],
+
+            ['This-is-the street 123', 'This-is-the street', '123'],
+            ['This-IS-the streetß 123', 'This-IS-the streetß', '123'],
+            ['This-äöü-the street 123', 'This-äöü-the street', '123'],
+        ];
+    }
+}
