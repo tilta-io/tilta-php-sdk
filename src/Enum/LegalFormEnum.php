4151b531b21251b1526bc2d6f7d5296e3531c57e
Frederik Rommel <frederik.rommel@webidea24.de>
2023-07-21 08:32:56 +0200

    add legal-form enum

---
+++ src/Enum/LegalFormEnum.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Copyright (c) Tilta Fintech GmbH
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Tilta\Sdk\Enum;
+
+class LegalFormEnum
+{
+    public const LEGAL_FORMS = [
+        'AG',
+        'EG',
+        'EK',
+        'EV',
+        'FREELANCER',
+        'GBR',
+        'GEWERBEBETRIEB',
+        'GMBH',
+        'GMBH_CO_KG',
+        'KDOR',
+        'KG',
+        'OHG',
+        'PRIVATE_PERSON',
+        'UG',
+    ];
+}
